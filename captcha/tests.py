e7cf18a653de83af5a251e8811bbc0b2b086e7f8
Dmitry Makushin <mdimanmail@gmail.com>
2013-09-05 11:17:15 +0400

    Patched tests - now no need internet connection

--- captcha/tests.py
+++ captcha/tests.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import mock
+import urllib2
 
 from captcha import fields
 from django.forms import Form
@@ -8,7 +10,22 @@ from django.forms import Form
 class TestForm(Form):
     captcha = fields.ReCaptchaField(attrs={'theme': 'white'})
 
+
 class TestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.urlopen = urllib2.urlopen
+        splitlines = mock.Mock()
+        splitlines.splitlines = mock.Mock(return_value=['false', 'invalid-request-cookie'])
+        read = mock.Mock(return_value=splitlines)
+        opener = mock.Mock()
+        opener.read = read
+        urllib2.urlopen = mock.Mock(return_value=opener)
+
+    @classmethod
+    def tearDownClass(cls):
+        urllib2.urlopen = cls.urlopen
 
     def setUp(self):
         os.environ['RECAPTCHA_TESTING'] = 'True'
@@ -21,6 +38,7 @@ class TestCase(unittest.TestCase):
     def test_envvar_disabled(self):
         os.environ['RECAPTCHA_TESTING'] = 'False'
         form_params = {'recaptcha_response_field': 'PASSED'}
+
         form = TestForm(form_params)
         self.assertFalse(form.is_valid())
 
